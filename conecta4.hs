32e818aedb6ccce3ba5fe969ab0f8ca4b679668b
Kevin Rodriguez <seidaikevin@gmail.com>
2021-10-18 17:14:00 -0500

    push

--- conecta4.hs
+++ conecta4.hs
@@ -1,4 +1,5 @@
 import Control.Monad
+import Data.Array
 import Data.List
 import System.IO
 
@@ -9,6 +10,9 @@ type Posiciones = [Posicion]
 data Marcador = Tab Posiciones Posiciones
   deriving (Show)
 
+data Marca = X | O
+  deriving (Show, Eq)
+
 muestraPosicion :: Marcador -> Posicion -> String
 muestraPosicion (Tab xs os) p
   | p `elem` xs = "X"
@@ -18,6 +22,32 @@ muestraPosicion (Tab xs os) p
 marcadorInicial :: Marcador
 marcadorInicial = Tab [] []
 
+tableroInicial :: [[a]]
+tableroInicial = replicate 7 []
+
+tableroPrueba = tableroInicial
+
+push columna marca tablero
+  | columna < 0 || columna >= 7 = tablero
+  | otherwise = y ++ x
+  where
+    (i, x) = splitAt columna tablero
+    y = tail (reverse i) ++ [last i ++ show marca]
+
+test =
+  push 2 X $ push 2 O tableroInicial
+
+listToLines l =
+  []
+
+-- tableroMod =
+-- map listToLines tableroPrueba
+
+-- >>> tableroInicial
+-- [[],[],[],[],[],[],[]]
+-- >>> listToLines tableroInicial
+-- []
+
 getRows :: Marcador -> Posiciones
 getRows (Tab row columns) = row
 
@@ -26,25 +56,26 @@ muestraLinea t =
   intercalate " | " . map (muestraPosicion t)
 
 -- >>> getRows marcadorInicial
--- Tab [] []
+-- []
 
 -- >>> muestraTablero marcadorInicial
--- "{1,2,3,4,5,6,71 | 2 | 3\n -+-+-\n4 | 5 | 6\n -+-+-\n7 | 8 | 9"
+-- "Numero de columna\n{1,2,3,4,5,6,7}\n--------------------\n[0,0,0,0,0,0,0]\n[0,0,0,0,0,0,0]\n[0,0,0,0,0,0,0]\n[0,0,0,0,0,0,0]\n[0,0,0,0,0,0,0]\n[0,0,0,0,0,0,0]\n--------------------\n"
 
 muestraTablero :: Marcador -> String
 muestraTablero marcador =
-  "Numero de columna\n" ++
-  "{" ++ intercalate "," (map show [1 .. 7]) ++ "}\n"
+  "Numero de columna\n"
+    ++ "{"
+    ++ intercalate "," (map show [1 .. 7])
+    ++ "}\n"
     ++ getSeparator
-    ++ intercalate "\n" (replicate 6 (show (replicate 7 0))) ++ "\n"
+    ++ intercalate "\n" (replicate 6 (show (replicate 7 0)))
+    ++ "\n"
     ++ getSeparator
-
 
 getSeparator :: [Char]
-getSeparator = 
+getSeparator =
   do
     concat (replicate 20 "-") ++ "\n"
-
 
 play :: IO ()
 play =
